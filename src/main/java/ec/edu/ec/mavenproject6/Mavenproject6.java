28651131df5831db49a02ef60f526abbbc7ed370
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-10-30 11:53:27 -0500

    comentario ssh

---
+++ src/main/java/ec/edu/ec/mavenproject6/Mavenproject6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.mavenproject6;
+
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
